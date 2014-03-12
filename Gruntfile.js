84d10609594f29224a5c158325032efc0c4cd719
efilnepo <MW1J6645@RU00081W.global-ad.net>
2014-03-12 11:54:28 +0100

    Added watch task for less folder of bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,7 +46,7 @@ module.exports = function (grunt) {
                 files: ['Gruntfile.js']
             },
             less: {
-                files: ['<%= config.app %>/styles/{,*/}*.less'],
+                files: ['<%= config.app %>/styles/{,*/}*.less', '<%= config.app %>/bower_components/bootstrap/less/{,*/}*.less'],
                 tasks: ['less:server', 'autoprefixer']
             },
             styles: {
